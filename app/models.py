e1c99a4b7c36b0b050dc7581f21537e970967f6d
阏男秀 <pl01665077@163.com>
2018-09-26 14:58:34 +0800

    update models

--- app/models.py
+++ app/models.py
@@ -9,13 +9,21 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app.extensions import db
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, index=True)
-    password_hash = db.Column(db.String(128))
+Model = db.Model
+Column = db.Column
+Integer = db.Integer
+String = db.String
+relationship = db.relationship
+
+
+class User(Model):
+    id = Column(Integer, primary_key=True)
+    username = Column(String(20), unique=True, index=True)
+    password_hash = Column(String(128))
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def validate_password(self, password):
         return check_password_hash(self.password_hash, password)
+
