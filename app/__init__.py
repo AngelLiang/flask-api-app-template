913cd9b2159f80520efc3b147e2de6429f3f427b
阏男秀 <pl01665077@163.com>
2018-09-26 14:35:44 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -11,16 +11,18 @@ from app.settings import config
 
 def create_app(config_name=None):
     if config_name is None:
-        config_name = os.getenv("FLASK_CONFIG", "development")
+        config_name = os.getenv('FLASK_CONFIG', 'development')
 
-    app = Flask("app")
+    app = Flask('app')
 
     # 从settings获取配置信息，加载到app.config里
     app.config.from_object(config[config_name])
 
     # 注册各种模块
     register_extensions(app)
-    register_blueprints(app)
+    register_apis(app)
+    register_shell_context(app)
+    register_commands(app)
 
     return app
 
@@ -29,6 +31,16 @@ def register_extensions(app):
     db.init_app(app)
 
 
-def register_blueprints(app):
+def register_apis(app):
     from app.apis.v1 import api_v1_bp
     app.register_blueprint(api_v1_bp, url_prefix="/api/v1")
+
+
+def register_shell_context(app):
+    @app.shell_context_processor
+    def make_shell_context():
+        return dict(db=db, User=User)
+
+
+def register_commands(app):
+    pass
