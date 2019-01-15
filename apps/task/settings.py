82b895852bc7a48467bc338c9736be4dbe9f63f6
AngelLiang <pl01665077@163.com>
2019-01-15 11:20:28 +0800

    feat: Updated task/settings & task/apis

--- apps/task/settings.py
+++ apps/task/settings.py
@@ -1,19 +1,30 @@
 # coding=utf-8
 
 import os
-from kombu import Exchange
+from kombu import Exchange, Queue
 
 BASEDIR = os.path.realpath(
     os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 )
 
+# ### 默认交换器配置 ###
+task_default_exchange = 'flask-api-app-tempalte'
+# task_default_routing_key = 'default'
+
 # ### 默认队列配置 ###
 task_default_queue = 'flask-api-app-tempalte'
-task_default_exchange = 'flask-api-app-tempalte'
-# task_default_routing_key = 'flask-api-app-tempalte'
-
 # delivery_mode: =1，message不写入磁盘；=2（默认）message会写入磁盘
 default_exchange = Exchange('flask-api-app-tempalte', delivery_mode=1)
+tasks_queues = (
+    Queue('async_task', default_exchange,
+          routing_key='default', auto_delete=True, durable=True),
+)
+# 配置路由
+task_routes = {
+    'async_task': {
+        'queue': 'async_task',
+    },
+}
 
 # ### 配置时区
 timezone = 'Asia/Shanghai'
@@ -29,6 +40,7 @@ worker_max_tasks_per_child = 10
 config_dict = dict(
     # flask调用任务似乎也需要指定该队列，否则没有发送到celery该默认队列
     # task_default_queue=task_default_queue,
+
     task_default_exchange=task_default_exchange,
     # task_default_routing_key=task_default_routing_key
     # tasks_queues=tasks_queues,
--- apps/web/task/apis.py
+++ apps/web/task/apis.py
@@ -12,3 +12,10 @@ def task_async():
     from apps.task.tasks import async_task
     async_task.delay()
     return jsonify(JsonResponse.success())
+
+
+@task_bp.route("/task/async2", methods=["GET", "POST"])
+def task_async2():
+    from apps.task.tasks import async_task
+    async_task.apply_async()
+    return jsonify(JsonResponse.success())
