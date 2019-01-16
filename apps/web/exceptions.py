b6d2cde595c7e2404bda943422dbe37f485f74fb
AngelLiang <pl01665077@163.com>
2019-01-16 11:22:59 +0800

    docs: Added annotations in apps/web/exceptions.py

--- apps/web/exceptions.py
+++ apps/web/exceptions.py
@@ -1,4 +1,15 @@
 # coding=utf-8
+"""
+
+- :exc:`Exception`
+    - :exc:`~apps.web.exceptions.APIException`
+        - :exc:`~apps.web.exceptions.FailException`
+        - :exc:`~apps.web.exceptions.NotFoundException`
+        - :exc:`~apps.web.exceptions.ParameterMissingException`
+        - :exc:`~apps.web.exceptions.ParameterErrorException`
+        - :exc:`~apps.web.exceptions.TokenTimeOutException`
+        - :exc:`~apps.web.exceptions.TokenErrorException`
+"""
 
 
 class APIException(Exception):
